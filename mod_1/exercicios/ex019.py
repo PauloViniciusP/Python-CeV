6b1075dee9a53b2f52758ea14525439e442c2809
Paulo Nascimento <nexz265@gmail.com>
2024-06-15 18:04:24 -0300

    Randomly selecting an item from the list

---
+++ mod_1/exercicios/ex019.py
@@ -0,0 +1,11 @@
+import random
+n1 = str(input('Primeiro aluno:'))
+n2 = str(input('Segundo aluno:'))
+n3 = str(input('Terceiro aluno:'))
+n4 = str(input('Quarto aluno:'))
+
+lista_nomes = [n1, n2, n3, n4]
+
+sorteado = random.choice(lista_nomes)
+
+print('O nome do aluno sorteado é {}'.format(sorteado))

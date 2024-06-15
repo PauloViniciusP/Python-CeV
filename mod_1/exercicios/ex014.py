45e3a07c76847c2c0a7f21152540c19cf812d8c1
Paulo Nascimento <nexz265@gmail.com>
2024-06-15 17:35:48 -0300

    Temperature converter

---
+++ mod_1/exercicios/ex014.py
@@ -0,0 +1,6 @@
+tempC = float(input('Digite a temperatura em °C:'))
+tempF = ((tempC/5)*9)+32
+print('A temperatura {}°C corresponde a {}°F'.format(tempC, tempF))
+
+n = int(input('Check stability: '))
+print(n)

ae35cce2d54dc71515ef9624d5575a61aa13f794
Paulo Nascimento <nexz265@gmail.com>
2024-06-18 13:36:14 -0300

    Updated to shorter code on print and added delay time

--- mod_1/exercicios/ex028.py
+++ mod_1/exercicios/ex028.py
@@ -1,11 +1,15 @@
 from random import randint
-print('-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=-')
+from time import sleep
+
+print('-=-' * 20)
 print("Adivinhe o número entre 0 e 5 que vou pensar. Tente a sorte!")
-print('-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=-')
+print('-=-' * 20)
 numero = randint(0, 5)
 
 palpite = int(input('Tente chutar o número entre 0 e 5 que eu pensei: '))
 
+print('Carregando...')
+sleep(2)
 if (palpite == numero):
     print('Acertou o número! Ambos pensamos em {}.'.format(palpite))
 else:

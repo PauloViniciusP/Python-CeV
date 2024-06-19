ac0c245b6da8a71f4b30d88394ba9f00e5c20f7d
Paulo Nascimento <nexz265@gmail.com>
2024-06-19 19:52:52 -0300

    Comparing numbers conditional

---
+++ mod_2/exercicios/ex038.py
@@ -0,0 +1,9 @@
+primeiro = int(input('Primeiro número: '))
+segundo = int(input('Segundo número: '))
+
+if primeiro > segundo:
+    print('O primeiro número é o maior.')
+elif primeiro < segundo:
+    print('O segundo número é o maior.')
+else:
+    print('Os números são iguais.')

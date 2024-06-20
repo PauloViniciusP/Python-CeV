d13177f173ec95d4e4d580335c3b5c3dffe5b0cf
Paulo Nascimento <nexz265@gmail.com>
2024-06-20 14:01:26 -0300

    Triangle analizer v2.1 - updated to nested conditional

--- mod_2/exercicios/ex042.py
+++ mod_2/exercicios/ex042.py
@@ -2,12 +2,14 @@ lado1 = float(input('Digite o tamanho do primeiro segmento de reta: '))
 lado2 = float(input('Digite o tamanho do segundo segmento de reta: '))
 lado3 = float(input('Digite o tamanho do terceiro segmento de reta: '))
 
-if lado1 >= lado2 + lado3 or lado2 >= lado1 + lado3 or lado3 >= lado1 + lado2:
-    print('É impossível formar um triângulo com os segmentos de reta informados.')
-elif lado1 == lado2 == lado3:
-    print('Os segmentos de reta acima podem formar um triângulo equilátero.')
-elif lado1 != lado2 and lado1 != lado3 and lado2 != lado3:
-    print('Os segmentos de reta acima podem formar um triângulo escaleno')
+if lado1 < lado2 + lado3 or lado2 < lado1 + lado3 or lado3 < lado1 + lado2:
+    print('É possível formar um triângulo ', end = '')
+    if lado1 == lado2 == lado3:
+        print('equilátero com os segmentos de reta informados.')
+    elif lado1 != lado2 != lado3 != lado1:
+        print('escaleno com os segmentos de reta informados.')
+    else:
+        print('isósceles com os segmentos de reta informados.')
 else:
-    print('Os segmentos de reta acima podem formar um triângulo isósceles.')
+    print('É impossível formar um triângulo com os segmentos de reta informados.')
     

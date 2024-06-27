e4e5b4081ab36b49fd7cca194f7302970721bdc0
Paulo Nascimento <nexz265@gmail.com>
2024-06-27 14:33:00 -0300

    Numbers written in full - tuple

---
+++ mod_3/exercicios/ex072.py
@@ -0,0 +1,22 @@
+nomeNum = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 
+           'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
+           'cartoze', 'quinze', 'dezesseis', 'dezessete', 'dezoito',
+           'dezenove', 'vinte')
+
+while True:
+    while True:
+        numero = int(input('Digite um número entere 0 e 20: '))
+        if 0 <= numero <= 20:
+            break
+        print('Tente novamente.', end='')
+
+    print(f'Você digitou o número {nomeNum[numero]}.')
+    print('-' * 30)
+    
+    escolha = ' '
+    while escolha not in 'SN':
+        escolha = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    
+    if escolha == 'N':
+        break
+print('Programa finalizado com sucesso!')

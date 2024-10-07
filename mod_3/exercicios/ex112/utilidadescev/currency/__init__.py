32f59d5b65b6b63efcebc330536086b33805e867
PauloViniciusP <nexz265@gmail.com>
2024-10-06 23:06:23 -0300

    Monetary data entry

---
+++ mod_3/exercicios/ex112/utilidadescev/currency/__init__.py
@@ -0,0 +1,42 @@
+def aumentar(preço=0, taxa=0, formatado=False):
+    """
+    -> Calcula o aumento de um determinado preço,
+       retornando o resultado com ou sem formatação.
+       :param preço: o preço a ser reajustado.
+       :param taxa: qual a porcentagem de aumento.
+       :param formatado: escolha entre saída formatada ou não.
+       :return: o valor reajustado, com ou sem formatação.
+    """
+    res = preço + (preço * taxa/100)
+    return res if formatado is False else moeda(res)
+
+
+def diminuir(preço=0, taxa=0, formatado=False):
+    res = preço - (preço * taxa/100)
+    return res if formatado is False else moeda(res)
+
+
+def dobro(preço=0, formatado=False):
+    res = preço * 2
+    return res if not formatado else moeda(res)
+
+
+def metade(preço=0, formatado=False):
+    res = preço / 2
+    return res if formatado is False else moeda(res)
+
+
+def moeda(preço=0, moeda='R$'):
+    return f'{moeda}{preço:>.2f}'.replace('.', ',')
+
+
+def resumo(preço=0, taxa_a=15, taxa_r=5):
+    print('-=' * 20)
+    print('Resumo do Valor'.center(40))
+    print('-=' * 20)
+    print(f'Preço analisado: \t{moeda(preço)}')
+    print(f'Dobro do preço: \t{dobro(preço, True)}')
+    print(f'Metade do preço: \t{metade(preço, True)}')
+    print(f'Com {taxa_a}% de aumento: \t{aumentar(preço, taxa_a, True)}')
+    print(f'Com {taxa_r}% de redução: \t{diminuir(preço, taxa_r, True)}')
+    print('-=' * 20)
---
+++ mod_3/exercicios/ex112/utilidadescev/data/__init__.py
@@ -0,0 +1,25 @@
+def leiaDinheiro(msg):
+    valido = False
+    while not valido:
+        entrada = str(input(msg)).replace(',', '.').strip()
+        if leiaInt(entrada):
+            if entrada.isalpha() or entrada == '':
+                print(f'\033[0;31mErro: \"{entrada}\" é um preço inválido!\033[m]')
+            else:
+                valido = True
+                return float(entrada)
+        
+
+def leiaInt(msg):
+    check = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            check = True
+        else:
+            print('\033[0;31mErro! Digite um número inteiro válido.\033[m')
+        if check:
+            break
+    return valor
